137e04705ce83f3d29b4abd34070c271f0008c1e
authon <authon@vip.qq.com>
2020-02-07 16:33:33 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,12 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/KFERMercer/luci-app-serverchan package/luci-app-serverchan
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/Lienol/openwrt-package package/openwrt-package
+
+./scripts/feeds update -a
+./scripts/feeds install -a
