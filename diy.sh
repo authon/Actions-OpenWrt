9a34f0633886827df0437a522b78d6fc1385739b
authon <authon@vip.qq.com>
2020-02-07 20:06:46 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -13,5 +13,18 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/Lienol/openwrt-package package/Lienol
 
+./scripts/feeds clean
 ./scripts/feeds update -a
+rm -rf feeds/lienol/lienol/ipt2socks
+rm -rf feeds/lienol/lienol/shadowsocksr-libev
+rm -rf feeds/lienol/lienol/pdnsd-alt
+rm -rf feeds/lienol/package/verysync
+rm -rf feeds/lienol/lienol/luci-app-verysync
+rm -rf package/lean/kcptun
+rm -rf package/lean/trojan
+rm -rf package/lean/v2ray
+rm -rf package/lean/luci-app-kodexplorer
+rm -rf package/lean/luci-app-pppoe-relay
+rm -rf package/lean/luci-app-pptp-server
+rm -rf package/lean/luci-app-v2ray-server
 ./scripts/feeds install -a
