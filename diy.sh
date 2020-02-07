317bc46b8278ae38802f8e1822ca9f9fb200d6a5
authon <authon@vip.qq.com>
2020-02-07 19:45:38 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,7 +11,7 @@ sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/KFERMercer/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-git clone https://github.com/Lienol/openwrt-package package/openwrt-package
+git clone https://github.com/Lienol/openwrt-package package/Lienol
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
